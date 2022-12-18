b318cb2ce552481197a73b8272d88faa689eb07e
alekslesik <alekslesik@gmail.com>
2022-12-18 12:54:59 +0300

    header on main page

--- components/intec.universe/main.header/template.1/templates/desktop/template.10/view.php
+++ components/intec.universe/main.header/template.1/templates/desktop/template.10/view.php
@@ -18,128 +18,145 @@ $arVisual = $arResult['VISUAL'];
 ?>
 <div class="widget-view-desktop-10">
     <?php if ($arResult['CONTACTS']['ADVANCED']) { ?>
-    <?php $sScheduleString = ''; ?>
-    <?php foreach ($arResult['CONTACTS']['VALUES'] as $arContact) {
+        <?php $sScheduleString = ''; ?>
+        <?php foreach ($arResult['CONTACTS']['VALUES'] as $arContact) {
             if (!empty($arContact['SCHEDULE'])) {
                 if (Type::isArray($arContact['SCHEDULE'])) {
                     foreach ($arContact['SCHEDULE'] as $sSchedule) {
-                        $sScheduleString .= $sSchedule.', ';
+                        $sScheduleString .= $sSchedule . ', ';
                     }
                 } else {
-                    $sScheduleString .= $arContact['SCHEDULE'].', ';
+                    $sScheduleString .= $arContact['SCHEDULE'] . ', ';
                 }
             }
         }
         $sScheduleString = substr($sScheduleString, 0, (strlen($sScheduleString) - 2));
         ?>
-    <span class="workhours">
-        <span class="value-title" title="<?=$sScheduleString?>"></span>
-    </span>
+        <span class="workhours">
+            <span class="value-title" title="<?= $sScheduleString ?>"></span>
+        </span>
     <?php
         unset($sScheduleString);
     } ?>
 
-    <?php //$APPLICATION->ShowViewContent('template-header-desktop-before') ?>
+    <?php //$APPLICATION->ShowViewContent('template-header-desktop-before')
+    ?>
     <div class="intec-content intec-content-primary intec-content-visible">
         <div class="intec-content-wrapper">
-            <div class="widget-container">
+            <div class="widget-container" style="padding-top: 15px; padding-bottom: 15px;">
                 <div class="intec-grid intec-grid-a-v-center intec-grid-i-h-16">
                     <?php if ($arResult['LOGOTYPE']['SHOW']['DESKTOP']) { ?>
-                    <div class="widget-logotype-container intec-grid-item-auto">
-                        <?= Html::beginTag($arResult['LOGOTYPE']['LINK']['USE'] ? 'a' : 'div', [
+                        <div class="widget-logotype-container intec-grid-item-auto">
+                            <?= Html::beginTag($arResult['LOGOTYPE']['LINK']['USE'] ? 'a' : 'div', [
                                 'href' => $arResult['LOGOTYPE']['LINK']['USE'] ? $arResult['LOGOTYPE']['LINK']['VALUE'] : null,
                                 'class' => [
                                     'widget-logotype',
                                     'intec-ui-picture'
                                 ],
                                 'style' => [
-                                    'width' => $arResult['LOGOTYPE']['DESKTOP']['WIDTH'].'px'
+                                    'width' => $arResult['LOGOTYPE']['DESKTOP']['WIDTH'] . 'px'
                                 ]
                             ]) ?>
-                        <?php include(__DIR__.'/../../../parts/logotype.php') ?>
-                        <?= Html::endTag($arResult['LOGOTYPE']['LINK']['USE'] ? 'a' : 'div') ?>
-                    </div>
+                            <?php include(__DIR__ . '/../../../parts/logotype.php') ?>
+                            <?= Html::endTag($arResult['LOGOTYPE']['LINK']['USE'] ? 'a' : 'div') ?>
+                        </div>
                     <?php } ?>
                     <?php if ($arResult['TAGLINE']['SHOW']['DESKTOP']) { ?>
-                    <div class="widget-tag-line-container intec-grid-item-auto">
-                        <div class="widget-tag-line">
-                            <?= $arResult['TAGLINE']['VALUE'] ?>
-                        </div>
-                    </div>
-                    <?php } ?>
-                    <?php if ($arResult['REGIONALITY']['USE']) { ?>
-                    <div class="widget-region-container intec-grid-item-auto">
-                        <div class="widget-region">
-                            <div class="widget-region-component">
-                                <?php $APPLICATION->IncludeComponent('intec.regionality:regions.select', $arResult['REGIONALITY']['TEMPLATE'], []) ?>
+                        <div class="widget-tag-line-container intec-grid-item-auto">
+                            <div class="widget-tag-line">
+                                <?= $arResult['TAGLINE']['VALUE'] ?>
                             </div>
                         </div>
-                    </div>
+                    <?php } ?>
+                    <?php if ($arResult['REGIONALITY']['USE']) { ?>
+                        <div class="widget-region-container intec-grid-item-auto">
+                            <div class="widget-region">
+                                <div class="widget-region-component">
+                                    <?php $APPLICATION->IncludeComponent('intec.regionality:regions.select', $arResult['REGIONALITY']['TEMPLATE'], []) ?>
+                                </div>
+                            </div>
+                        </div>
                     <?php } ?>
                     <div class="intec-grid-item"></div>
                     <?php if ($arResult['CONTACTS']['SHOW']['DESKTOP'] || $arResult['FORMS']['CALL']['SHOW']) { ?>
-                    <div class="widget-information-container intec-grid-item-auto">
-                        <div class="intec-grid intec-grid-a-v-center intec-grid-i-h-8">
-                            <?php if ($arResult['CONTACTS']['SHOW']['DESKTOP']) {
-                                    include(__DIR__.'/parts/contacts.php');
+                        <div class="widget-information-container intec-grid-item-auto" style="flex: 1 0 auto;">
+                            <div class="intec-grid intec-grid-a-v-center intec-grid-i-h-8" style="display: flex; align-content: center; justify-content: space-around;">
+                                <?php if ($arResult['CONTACTS']['SHOW']['DESKTOP']) {
+                                    include(__DIR__ . '/parts/contacts.php');
                                 } ?>
-                            <?php //if ($arResult['FORMS']['CALL']['SHOW']) { ?>
-                            <div class="widget-call-container intec-grid-item-auto">
-                                <?= Html::tag('div', Loc::getMessage('C_HEADER_TEMP1_DESKTOP_TEMP10_BUTTON'), [
+                                <?php if ($arResult['FORMS']['CALL']['SHOW']) { ?>
+                                    <div hidden class="widget-call-container intec-grid-item-auto">
+                                        <?= Html::tag('div', Loc::getMessage('C_HEADER_TEMP1_DESKTOP_TEMP10_BUTTON'), [
                                             'class' => [
                                                 'widget-call',
                                                 'intec-cl-text'
                                             ],
                                             'data-action' => 'forms.call.open'
                                         ]) ?>
-                                <?php include(__DIR__.'/../../../parts/forms/call.php') ?>
-                            </div>
-                            <?php //} ?>
-                            <div class="widget-call-container intec-grid-item-auto" style="color: #ffffff;">
-                                <span class="adress"><?= $arContact['ADDRESS'] ?></span>
+                                        <?php include(__DIR__ . '/../../../parts/forms/call.php') ?>
+                                    </div>
+                                <?php } ?>
+                                <div class="widget-contacts-container intec-grid-item-auto" style="color: #ffffff;">
+                                    <div style="margin-bottom: 5px;">
+                                        <i class="fa-solid fa-phone" style="margin-right: 7px; color:#7cc842"></i>
+                                        <a style="color: #7cc842; font-weight: 700; font-size: 1.1em;" href="tel:<?= $arContactItem['PHONE']['VALUE'] ?>" class="tel">
+                                            <span class="value"><?= $arContactItem['PHONE']['DISPLAY'] ?></span>
+                                        </a>
+                                    </div>
+                                    <div class="wrapper" style="display: flex; align-items: center;">
+                                        <i class="fa-solid fa-envelope" style="margin-right: 7px; color:#7cc842"></i>
+                                        <div class="adress" style="display: inline-block;">
+                                            <div class="adress">Юридическим лицам</div>
+                                            <a style="color: #7cc842" href="mailto:<?= $arContactItem["EMAIL"] ?>">
+                                                <span class="value"><?= $arContactItem["EMAIL"] ?></span>
+                                            </a>
+                                        </div>
+                                    </div>
+
+                                </div>
                             </div>
                         </div>
-                    </div>
                     <?php } ?>
                     <?php if ($arResult['SEARCH']['SHOW']['DESKTOP']) { ?>
-                    <div class="widget-search-container intec-grid-item-auto">
-                        <div class="widget-search">
-                            <?php $arSearchParams = [
-                                    'INPUT_ID' => $arParams['SEARCH_INPUT_ID'].'-desktop'
+                        <div class="widget-search-container intec-grid-item-auto" hidden>
+                            <div class="widget-search">
+                                <?php $arSearchParams = [
+                                    'INPUT_ID' => $arParams['SEARCH_INPUT_ID'] . '-desktop'
                                 ] ?>
-                            <?php include(__DIR__.'/../../../parts/search/popup.1.php') ?>
+                                <?php include(__DIR__ . '/../../../parts/search/popup.1.php') ?>
+                            </div>
                         </div>
-                    </div>
                     <?php } ?>
                     <?php if ($arResult['AUTHORIZATION']['SHOW']['DESKTOP']) { ?>
-                    <div hidden class="widget-authorization-container intec-grid-item-auto">
-                        <div class="widget-authorization">
-                            <?php include(__DIR__.'/../../../parts/auth/panel.4.php') ?>
+                        <div hidden class="widget-authorization-container intec-grid-item-auto">
+                            <div class="widget-authorization">
+                                <?php include(__DIR__ . '/../../../parts/auth/panel.4.php') ?>
+                            </div>
                         </div>
-                    </div>
                     <?php } ?>
                     <?php if ($arResult['BASKET']['SHOW']['DESKTOP'] || $arResult['DELAY']['SHOW']['DESKTOP'] || $arResult['COMPARE']['SHOW']['DESKTOP']) { ?>
-                    <div class="widget-basket-container intec-grid-item-auto">
-                        <div class="widget-basket">
-                            <?php include(__DIR__.'/../../../parts/basket.php') ?>
+                        <div class="widget-basket-container intec-grid-item-auto">
+                            <div class="widget-basket">
+                                <?php include(__DIR__ . '/../../../parts/basket.php') ?>
+                            </div>
                         </div>
-                    </div>
                     <?php } ?>
                 </div>
             </div>
         </div>
     </div>
     <?php if ($arVisual['TRANSPARENCY']) { ?>
-    <div class="intec-content intec-content-primary intec-content-visible">
-        <div class="intec-content-wrapper">
+        <div class="intec-content intec-content-primary intec-content-visible">
+            <div class="intec-content-wrapper">
             <?php } ?>
             <div class="widget-menu">
                 <?php $arMenuParams = ['TRANSPARENT' => 'N'] ?>
-                <?php include(__DIR__.'/../../../parts/menu/main.horizontal.1.php') ?>
+                <?php include(__DIR__ . '/../../../parts/menu/main.horizontal.1.php') ?>
             </div>
             <?php if ($arVisual['TRANSPARENCY']) { ?>
+            </div>
         </div>
-    </div>
     <?php } ?>
-    <?php //$APPLICATION->ShowViewContent('template-header-desktop-after') ?>
+    <?php //$APPLICATION->ShowViewContent('template-header-desktop-after')
+    ?>
 </div>
