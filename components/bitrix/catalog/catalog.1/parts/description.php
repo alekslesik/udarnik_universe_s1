6a601665feafb04db233d5cc2e79a9aa6748c9d0
alekslesik <alekslesik@gmail.com>
2022-12-30 12:28:35 +0300

    modile version

--- components/bitrix/catalog/catalog.1/parts/description.php
+++ components/bitrix/catalog/catalog.1/parts/description.php
@@ -70,4 +70,53 @@
         font-weight: 700;
         text-align: right;
     }
+
+    @media (max-width: 720px) {
+
+        .my-flex-box:nth-child(1) {
+            flex: 0 1 65%;
+            margin-right: 40px;
+            margin-top: 40px;
+        }
+
+        .my-flex-box-img:nth-child(2) {
+            display: none;
+        }
+
+        .my-flex-cont-icons {
+            display: none;
+        }
+
+        .-img {
+            flex-wrap: wrap;
+            display: flex;
+            justify-content: center;
+            background-color: #393c4b;
+            color: #ffffff;
+            margin-top: 30px;
+            padding-bottom: 50px;
+            width: 100%;
+        }
+
+        .my-flex-box-img li p {
+            width: 90%;
+        }
+
+        table.iksweb {
+            text-decoration: none;
+            border-collapse: collapse;
+            width: auto;
+        }
+
+        .my-flex-box:nth-child(2) {
+            margin-top: 80px;
+            margin-left: 0;
+            flex: auto;
+            margin-left: 75px;
+        }
+
+        .-img::after {
+            display: none;
+        }
+    }
 </style>
